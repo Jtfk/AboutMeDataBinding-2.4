ffbfef833acfb4c3a63207b58985fc20bad9a4ff
Jtfk <Jtfk@users.noreply.github.com>
2021-07-05 15:22:53 +0800

    add practical 2.4

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,47 +1,47 @@
 package com.example.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Jtfk")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 
+        binding.nicknameEdit.requestFocus()
 
-        val btnDone: Button = findViewById(R.id.btnDone)
-        val tvNickName: EditText = findViewById(R.id.nickname_edit)
-        val tvResult: TextView = findViewById(R.id.nickname_text)
+        binding.btnDone.setOnClickListener() {
 
-        tvNickName.requestFocus()
-
-        btnDone.setOnClickListener() {
-
-            tvResult.text = tvNickName.text.toString()
-
-            tvNickName.visibility = View.GONE
-            btnDone.visibility = View.GONE
-            tvResult.visibility = View.VISIBLE
+            binding.apply {
+                myName?.nickname = nicknameEdit.text.toString()
+                invalidateAll()
+                nicknameEdit.visibility = View.GONE
+                btnDone.visibility = View.GONE
+                nicknameText.visibility = View.VISIBLE
+            }
 
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(it.windowToken, 0)
         }
 
-        tvResult.setOnClickListener() {
-            tvNickName.visibility = View.VISIBLE
-            btnDone.visibility = View.VISIBLE
-            tvResult.visibility = View.GONE
-            tvNickName.requestFocus()
-            tvNickName.setSelection(tvNickName.length())
+        binding.nicknameText.setOnClickListener() {
+            binding.nicknameEdit.visibility = View.VISIBLE
+            binding.btnDone.visibility = View.VISIBLE
+            binding.nicknameText.visibility = View.GONE
+            binding.nicknameEdit.requestFocus()
+            binding.nicknameEdit.setSelection(binding.nicknameText.length())
 
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(tvNickName, 0)
+            imm.showSoftInput(binding.nicknameEdit, 0)
         }
 
 
